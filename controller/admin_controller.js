ee67ede5215b514bcd3426344b546671ecf41852
tush87 <tusharj8788@gmail.com>
2023-05-20 09:43:57 +0530

    add create admin controller function

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -3,19 +3,42 @@ const AdminDB = require('../models/admin');
 //create admin [for future use]
 module.exports.createAdmin =async function(req,res){
     try{
+        let {email,password,confirmPassword} = req.body;
         
+        //password and confirm password match
+        if(password != confirmPassword){
+            console.log('password and confirm password not match');
+            return res.redirect('back');
+        }
+
+        let admin = await AdminDB.find({});
+
+        //check any entry already created in db then just back
+        if(admin.length>0){
+            console.log('Admin already exist');
+            return res.redirect('/signin');
+        }
+
+        //create new admin
+        admin = await AdminDB.create(req.body);
+        console.log("Admin create successfully");
+        return res.redirect('/signin');
+
     }
     catch(err){
-
+        console.log(err);
+        return res.redirect('back');
     }
 }
 
 //create session
 module.exports.createSession =async function(req,res){
     try{
-
+        console.log(req.body);
+        
     }
     catch(err){
-
+        console.log(err);
+        return res.redirect('back');
     }
 }

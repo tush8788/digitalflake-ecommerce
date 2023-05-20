2be935878a1b3df8f160ecc34bd3308f7c66c969
tush87 <tusharj8788@gmail.com>
2023-05-20 11:00:25 +0530

    add category models

---
+++ models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    discription:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['Active','Inactive']
+    }
+},{
+    timestamps:true
+});
+
+const Category = mongoose.model('Category',categorySchema);
+
+module.exports = Category;

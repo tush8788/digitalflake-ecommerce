8ddbc4d464be54c34ae64ba54c8b2411df7bf096
tush87 <tusharj8788@gmail.com>
2023-05-20 16:44:58 +0530

    add nodemailer config file

---
+++ config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+const path = require('path');
+const ejs = require('ejs');
+
+//configration
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // two factor auth 
+    auth: {
+      user: process.env.email || 'programming.tushar@gmail.com', // generated ethereal user
+      pass: process.env.password ||'hmdxlfqkuugsnwiw', // generated ethereal password
+    },
+  });
+
+//send html
+let renderTemplete = (data,relativePath)=>{
+    let mailHtml;
+    ejs.renderFile(path.join(__dirname,'../views/mailer',relativePath),
+    data,function(err,templete){
+        if(err){
+            console.log('error in rendering templete');
+            return;
+        }
+        mailHtml = templete;
+    });
+
+    return mailHtml;
+}
+
+module.exports={
+    transporter:transporter,
+    renderTemplete:renderTemplete
+}

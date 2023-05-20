637e5e1a6a823d2b003dc3a89d9b2bf78634f860
tush87 <tusharj8788@gmail.com>
2023-05-20 14:01:00 +0530

    add addProduct.ejs and router

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -1,4 +1,5 @@
 const ProductDB = require('../models/product');
+const CategoryDB = require('../models/category');
 
 //product show
 module.exports.showProduct = async function(req,res){
@@ -8,6 +9,33 @@ module.exports.showProduct = async function(req,res){
             title:"Products",
             products
         })
+    }
+    catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
+}
+
+//create product page
+module.exports.createProductPage = async function(req,res){
+    try{
+        //find all category
+        let categorise = await CategoryDB.find({status:"Active"});
+
+        return res.render('products/addProduct',{
+            title:"Add Product",
+            categorise
+        })
+    }
+    catch(err){
+
+    }
+}
+
+//create product
+module.exports.createProduct = async function(req,res){
+    try{
+        console.log(req.body);
     }
     catch(err){
         console.log(err);
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,12 @@ const passport = require('passport');
 const productController = require('../controller/product_controller');
 const router = express.Router();
 
+//show all products
 router.get('/',passport.checkAuthenticationUser,productController.showProduct);
+
+//create product page
+router.get('/create',passport.checkAuthenticationUser,productController.createProductPage);
+
+router.post('/create',passport.checkAuthenticationUser,productController.createProduct);
 
 module.exports = router;

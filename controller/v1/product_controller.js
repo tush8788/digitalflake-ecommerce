5ba0b6c16be97a3e4311875fc777963cdc11afe6
tush87 <tusharj8788@gmail.com>
2023-05-21 13:47:01 +0530

    add delete product func using api

--- controller/v1/product_controller.js
+++ controller/v1/product_controller.js
@@ -1,9 +1,18 @@
 const ProductDB = require('../../models/product');
+const fs = require('fs');
+const path = require('path');
 
 // view products
 module.exports.viewProduct = async function(req,res){
     try{
-        let products = await ProductDB.find({});
+        let products;
+        //if admin send product id 
+        if(req.query.id){
+            products = await ProductDB.find({_id:req.query.id});
+        }
+        else{
+            products = await ProductDB.find({});
+        }
         
         return res.status(200).json({
             message:"All products",
@@ -11,6 +20,52 @@ module.exports.viewProduct = async function(req,res){
         })
     }
     catch(err){
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
+
+//delete product 
+module.exports.deleteProduct = async function(req,res){
+    try{
+        let {id}=req.query;
+
+        //check admin send product id or not
+        if(!id){
+            return res.status(300).json({
+                message:"product id not found"
+            })
+        }
+        
+        //find product in DB
+        let product = await ProductDB.findById(id);
+        
+        //if product not found in DB
+        if(!product){
+            return res.status(300).json({
+                message:"product id not found"
+            })
+        }
+
+        //first delete img of product 
+        if(product.img){
+            //check given location file exist or not
+            if(fs.existsSync(path.join(__dirname,'../../',product.img))){
+                //delete file
+                fs.unlinkSync(path.join(__dirname,'../../',product.img)); 
+            }
+        }
+
+        //delete product 
+        await product.deleteOne();
+        
+        return res.status(200).json({
+            message:"product delete successfully",
+        })
+    }
+    catch(err){
+        console.log(err);
         return res.status(500).json({
             message:"Internal server error"
         })
--- routes/v1/product.js
+++ routes/v1/product.js
@@ -3,9 +3,13 @@ const productController = require('../../controller/v1/product_controller');
 const passport = require('passport');
 const router = express.Router();
 
+//view all or view specific product
 router.get('/',passport.authenticate(
     'jwt',
     {session:false}
 ),productController.viewProduct);
+
+//delete product
+router.delete('/',passport.authenticate('jwt',{session:false}),productController.deleteProduct);
 
 module.exports = router;
